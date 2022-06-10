b254372b955d5e53b6dc68937be432ff3e5bc83c
jmg <jmg41490400@gmail.com>
2022-06-10 23:34:25 +0900

    controller와 service 분리 이유

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// 요청과 응답을 처리
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-
+// 요청과 응답에 상관없이 이루어지는 로직
+// 협업을 위해 구조를 강제화
+// 요청응답, 비즈니스로직을 분리
 @Injectable()
 export class AppService {
   getHello(): string {
