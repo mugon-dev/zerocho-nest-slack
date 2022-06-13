be806d862125ce4eab815c20f36e59553f812fb0
jmg <jmg41490400@gmail.com>
2022-06-13 14:10:30 +0900

    adjust transaction

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import bcrypt from 'bcrypt';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Users } from '../entities/Users.entity';
 import { WorkspaceMembers } from '../entities/WorkspaceMembers.entity';
 import { ChannelMembers } from '../entities/ChannelMembers.entity';
@@ -14,6 +14,7 @@ export class UsersService {
     private workspaceMembersRepository: Repository<WorkspaceMembers>,
     @InjectRepository(ChannelMembers)
     private channelMembersRepository: Repository<ChannelMembers>,
+    private dataSource: DataSource,
   ) {}
 
   async getUser(email: string) {
@@ -21,24 +22,35 @@ export class UsersService {
   }
 
   async join(email: string, nickname: string, password: string) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
     const user = await this.usersRepository.findOne({ where: { email } });
     if (user) {
       // 이미 존재하는 유저라고 에러
       throw new UnauthorizedException('이미 존재하는 사용자입니다.');
     }
     const hashedPassword = await bcrypt.hash(password, 12);
-    const returned = await this.usersRepository.save({
-      email,
-      nickname,
-      password: hashedPassword,
-    });
-    await this.workspaceMembersRepository.save({
-      UserId: returned.id,
-      WorkspaceId: 1,
-    });
-    await this.channelMembersRepository.save({
-      UserId: returned.id,
-      ChannelId: 1,
-    });
+    // transaction
+    try {
+      const returned = await queryRunner.manager.getRepository(Users).save({
+        email,
+        nickname,
+        password: hashedPassword,
+      });
+      await queryRunner.manager.getRepository(WorkspaceMembers).save({
+        UserId: returned.id,
+        WorkspaceId: 1,
+      });
+      await queryRunner.manager.getRepository(ChannelMembers).save({
+        UserId: returned.id,
+        ChannelId: 1,
+      });
+      await queryRunner.commitTransaction();
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
