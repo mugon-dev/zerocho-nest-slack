8e184ea8268dd5111d691db63d13bee477cb34d7
jmg <jmg41490400@gmail.com>
2022-06-13 14:01:48 +0900

    add join logic

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,11 +3,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 import bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { Users } from '../entities/Users.entity';
+import { WorkspaceMembers } from '../entities/WorkspaceMembers.entity';
+import { ChannelMembers } from '../entities/ChannelMembers.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users) private usersRepository: Repository<Users>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
   ) {}
 
   async getUser(email: string) {
@@ -21,10 +27,18 @@ export class UsersService {
       throw new UnauthorizedException('이미 존재하는 사용자입니다.');
     }
     const hashedPassword = await bcrypt.hash(password, 12);
-    await this.usersRepository.save({
+    const returned = await this.usersRepository.save({
       email,
       nickname,
       password: hashedPassword,
+    });
+    await this.workspaceMembersRepository.save({
+      UserId: returned.id,
+      WorkspaceId: 1,
+    });
+    await this.channelMembersRepository.save({
+      UserId: returned.id,
+      ChannelId: 1,
     });
   }
 }
