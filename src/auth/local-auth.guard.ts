dcaf776e4fde76c10321db2dbfd7c37ada90d2e2
jmg <jmg41490400@gmail.com>
2022-06-13 14:25:15 +0900

    fix create session error

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -3,10 +3,17 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
-  async canActive(context: ExecutionContext): Promise<boolean> {
-    console.log('use auth guard');
-    const result = (await super.canActivate(context)) as boolean;
-    await super.logIn(context.switchToHttp().getRequest());
-    return result;
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const can = await super.canActivate(context);
+    if (can) {
+      const request = context.switchToHttp().getRequest();
+      console.log('login for cookie');
+      await super.logIn(request);
+    }
+    return true;
+    // 다른 방법
+    // const result = (await super.canActivate(context)) as boolean;
+    // await super.logIn(context.switchToHttp().getRequest());
+    // return result;
   }
 }
