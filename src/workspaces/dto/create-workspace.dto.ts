e17728d5a64ff38a2007cb287e8a620e035d4e4c
jmg <jmg41490400@gmail.com>
2022-06-13 16:00:48 +0900

    create workspace module api with controller, service

---
+++ src/workspaces/dto/create-workspace.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateWorkspaceDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '슬리액트',
+    description: '워크스페이스명',
+  })
+  public workspace: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 'sleact',
+    description: 'url 주소',
+  })
+  public url: string;
+}
--- src/workspaces/workspaces.module.ts
+++ src/workspaces/workspaces.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { WorkspacesController } from './workspaces.controller';
 import { WorkspacesService } from './workspaces.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Workspaces } from '../entities/Workspaces.entity';
+import { Repository } from 'typeorm';
+import { Channels } from '../entities/Channels.entity';
+import { WorkspaceMembers } from '../entities/WorkspaceMembers.entity';
+import { ChannelMembers } from '../entities/ChannelMembers.entity';
+import { Users } from '../entities/Users.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Workspaces,
+      Repository,
+      Channels,
+      WorkspaceMembers,
+      ChannelMembers,
+      Users,
+    ]),
+  ],
   controllers: [WorkspacesController],
   providers: [WorkspacesService],
 })
